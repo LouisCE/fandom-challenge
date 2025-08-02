43eb61c761674693a6269096585a0c576e9972b6
LouisCE <louiscowell190@yahoo.co.uk>
2025-08-02 21:35:49 +0100

    Add Avatar: The Last Airbender quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+// Avatar: The Last Airbender questions
+const avatarQuestions = [
+    {
+        question: "What is the name of Aang’s flying bison?",
+        answers: ["Appa", "Momo", "Naga", "Pabu"],
+        correct: 0
+    },
+    {
+        question: "Which nation was the first to fall to the Fire Nation?",
+        answers: ["Air Nomads", "Earth Kingdom", "Water Tribe", "Fire Nation"],
+        correct: 0
+    }
+];
