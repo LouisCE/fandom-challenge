5b667ca6fc699a0982f9188ad2ee3a23b19721c3
LouisCE <louiscowell190@yahoo.co.uk>
2025-08-03 17:21:58 +0100

    Get question and answer button elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,11 @@ alert("Welcome to Fandom Challenge!");
 const startBtn = document.getElementById('start-btn');
 const rulesSection = document.getElementById('rules');
 const quizSection = document.getElementById('quiz');
+const questionText = document.getElementById('question-text');
+const answerButtons = document.querySelectorAll('.answer-btn');
 
 // When Start Quiz is clicked
 startBtn.addEventListener('click', function() {
-    rulesSection.style.display = 'none';   // Hide rules
-    quizSection.style.display = 'block';   // Show quiz
+    rulesSection.style.display = 'none';
+    quizSection.style.display = 'block';
 });
