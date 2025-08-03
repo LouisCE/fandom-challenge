d446fc47687178531fb0b7e75b154954d26765b4
LouisCE <louiscowell190@yahoo.co.uk>
2025-08-03 17:27:58 +0100

    Add variable to track current question index

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ const quizSection = document.getElementById('quiz');
 const questionText = document.getElementById('question-text');
 const answerButtons = document.querySelectorAll('.answer-btn');
 
+// Track which question the user is on
+let currentQuestionIndex = 0;
+
 // When Start Quiz is clicked
 startBtn.addEventListener('click', function() {
     rulesSection.style.display = 'none';
