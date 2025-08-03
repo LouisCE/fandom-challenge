ee7464fe3042c651aea8d99c205280a597dd6c40
LouisCE <louiscowell190@yahoo.co.uk>
2025-08-03 03:02:41 +0100

    Add basic Start Quiz button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 console.log("JavaScript is connected!");
 
 alert("Welcome to Fandom Challenge!");
+
+// Get elements
+const startBtn = document.getElementById('start-btn');
+const rulesSection = document.getElementById('rules');
+const quizSection = document.getElementById('quiz');
+
+// When Start Quiz is clicked
+startBtn.addEventListener('click', function() {
+    rulesSection.style.display = 'none';   // Hide rules
+    quizSection.style.display = 'block';   // Show quiz
+});
