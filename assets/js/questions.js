57470adc4a9061bce530f4ea2c8112ebfa6d5666
LouisCE <louiscowell190@yahoo.co.uk>
2025-08-04 07:08:17 +0100

    Add 10 Avatar and 10 GoT questions to questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -9,19 +9,99 @@ const avatarQuestions = [
         question: "Which nation was the first to fall to the Fire Nation?",
         answers: ["Air Nomads", "Earth Kingdom", "Water Tribe", "Fire Nation"],
         correct: 0
+    },
+    {
+    question: "Who is the Avatar before Aang?",
+    answers: ["Roku", "Kyoshi", "Kuruk", "Yangchen"],
+    correct: 0
+    },
+    {
+        question: "What element did Aang struggle to learn the most?",
+        answers: ["Fire", "Earth", "Water", "Air"],
+        correct: 1
+    },
+    {
+        question: "Who teaches Aang firebending after Zuko joins Team Avatar?",
+        answers: ["Iroh", "Jeong Jeong", "Zuko", "Azula"],
+        correct: 2
+    },
+    {
+        question: "What is the name of Sokka's boomerang?",
+        answers: ["It doesn't have a name", "Boomy", "Whirly", "Boomer"],
+        correct: 0
+    },
+    {
+        question: "Which spirit helps Aang in the Spirit World as a guide?",
+        answers: ["Koh", "Hei Bai", "Roku's Dragon", "Wan Shi Tong"],
+        correct: 2
+    },
+    {
+        question: "What is the name of Toph’s wrestling persona?",
+        answers: ["The Blind Bandit", "The Boulder", "Toph Smash", "Metal Queen"],
+        correct: 0
+    },
+    {
+        question: "Who captures Appa in Ba Sing Se?",
+        answers: ["Dai Li", "Sandbenders", "Azula", "Long Feng"],
+        correct: 3
+    },
+    {
+        question: "What is the technique Aang uses to defeat Ozai?",
+        answers: ["Avatar State", "Energybending", "Chi Blocking", "Bloodbending"],
+        correct: 1
     }
 ];
 
 // Game of Thrones questions
 const gotQuestions = [
     {
+        question: "What is the motto of House Stark?",
+        answers: ["Fire and Blood", "Ours is the Fury", "Winter is Coming", "Hear Me Roar"],
+        correct: 2
+    },
+    {
+        question: "Who kills the Night King?",
+        answers: ["Jon Snow", "Arya Stark", "Bran Stark", "Daenerys Targaryen"],
+        correct: 1
+    },
+    {
+        question: "What is the name of Arya’s sword?",
+        answers: ["Needle", "Ice", "Oathkeeper", "Longclaw"],
+        correct: 0
+    },
+    {
+        question: "Which dragon survives to the end of the series?",
+        answers: ["Drogon", "Rhaegal", "Viserion", "None"],
+        correct: 0
+    },
+    {
+        question: "Who is known as 'The Kingslayer'?",
+        answers: ["Robert Baratheon", "Stannis Baratheon", "Jaime Lannister", "Sandor Clegane"],
+        correct: 2
+    },
+    {
+        question: "What is the name of Daenerys’s first husband?",
+        answers: ["Daario Naharis", "Khal Drogo", "Hizdahr zo Loraq", "Jon Snow"],
+        correct: 1
+    },
+    {
+        question: "Who poisons King Joffrey at his wedding?",
+        answers: ["Olenna Tyrell", "Petyr Baelish", "Sansa Stark", "Tyrion Lannister"],
+        correct: 0
+    },
+    {
         question: "Who killed Roose Bolton?",
         answers: ["Ramsay Bolton", "Jon Snow", "Walder Frey", "Poisoned by his enemies"],
-        correct: 1
+        correct: 0
     },
     {
         question: "What is the name of Jon Snow’s direwolf?",
         answers: ["Ghost", "Nymeria", "Summer", "Shaggydog"],
         correct: 0
-    }
+    },
+    {
+    question: "Who sits on the Iron Throne at the end of Game of Thrones?",
+    answers: ["Jon Snow", "Sansa Stark", "Bran Stark", "Daenerys Targaryen"],
+    correct: 2
+    },
 ];
